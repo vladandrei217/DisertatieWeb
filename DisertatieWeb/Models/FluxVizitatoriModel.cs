5c0882223c09ce7ab3e94dc6fdb4b71ddf213bdc
Vlad Petcu <petcu.vlad@yahoo.com>
2025-04-28 20:13:27 +0300

    Completare meniuri necesare

---
+++ DisertatieWeb/Models/FluxVizitatoriModel.cs
@@ -0,0 +1,36 @@
+﻿namespace DisertatieWeb.Models
+{
+    public class FluxVizitatoriModel
+    {
+        public string Locație { get; set; }
+        public int NumarVizitatori { get; set; }
+        public DateTime OraRaportarii { get; set; }
+    }
+    public class ZonaPopularaModel
+    {
+        public int Id { get; set; }
+        public string NumeZona { get; set; }
+        public int Vizitatori { get; set; }
+        public double Latitudine { get; set; }
+        public double Longitudine { get; set; }
+    }
+    public class PunctDeInteresModel
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public string Descriere { get; set; }
+        public double Latitudine { get; set; }
+        public double Longitudine { get; set; }
+        public string ImagineUrl { get; set; }
+    }
+    public class InteractiuneSenzorModel
+    {
+        public int Id { get; set; }
+        public string PunctDeInteres { get; set; }
+        public bool NotificareTrimisa { get; set; }
+        public bool ActivareLumini { get; set; }
+        public bool RedareMesajAudio { get; set; }
+    }
+
+
+}
--- DisertatieWeb/wwwroot/mapa.js
+++ DisertatieWeb/wwwroot/mapa.js
@@ -22,3 +22,17 @@
         });
     });
 };
+window.initZoneMap = function (zonePopulare) {
+    const map = L.map('zoneMap').setView([44.4268, 26.1025], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+    }).addTo(map);
+
+    zonePopulare.forEach(zona => {
+        L.marker([zona.latitudine, zona.longitudine])
+            .addTo(map)
+            .bindPopup(`${zona.numeZona}<br>Vizitatori: ${zona.vizitatori}`);
+    });
+};
+
